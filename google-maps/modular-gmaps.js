52206964a1121893ee798cd87cdb16b4f8a4c723
Rob Lum <lum.robert@gmail.com>
2014-10-22 17:30:28 -0400

    implemented 'jsonp' through javascript w/ instagram api endpoint for now; _ajax returns object with 2 arrays (positive latitude; negative latitude coors)

--- google-maps/modular-gmaps.js
+++ google-maps/modular-gmaps.js
@@ -1,23 +1,58 @@
 (function(){
-    var mapConfigurations = (function(campaign, center, zoom, heat){
+    var mapConfigurations = (function(){
+
         var mapOptions;
 
-        var configureMap = function(campaign, center, zoom, heat){
+        var configureMap = function(campaign, zoom, center, optional){
+            var heat = false;
+
             mapOptions = {
                 zoom : zoom
                 ,center : new google.maps.LatLng(center.lat, center.lng)
             }
 
-            return mapOptions
         }
-        
 
         var googleMap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+        var _ajax = (function(){
+            var baseURL
+                , apiKey
+                , positive = []
+                , negative = [];
+
+            function jsonp(url, callback) {
+                var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+                window[callbackName] = function(data) {
+                    delete window[callbackName];
+                    document.body.removeChild(script);
+                    callback(data);
+                };
+
+                var script = document.createElement('script');
+                script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+                document.body.appendChild(script);
+            }
+
+            jsonp('https://api.instagram.com/v1/users/276609664/media/recent/?client_id=df70d4f39d3649a9b724876a0f2de343', function(data) {
+                console.log(data);
+                    // Send data to negative or positive arrays based on latitude
+
+            });
+
+            return {
+                pos : positive
+                , neg : negative
+            };
+
+
+        })();
+
         return {
-            configureMap : configureMap 
+            configureMap : configureMap
         }
 
     });
+
 
 })();
