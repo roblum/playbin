3c4aad741b0676d68c46801fa82c5738f95ef4a7
Rob Lum <lum.robert@gmail.com>
2014-10-27 16:54:43 -0400

    code for client to embed into webpage to create map widget

---
+++ google-maps/client-embed.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    OPPixelInfo = {
+          campaign : 'CAMPAIGN_ID',
+          zoom: '13',
+          center : 'latest',
+          styles : 'styles go here'
+
+    };
+
+    var e = document.createElement('script');
+    e.src = 'https://offerpop.com/gmaps-visualization.js';
+    e.async = true;
+    document.body.appendChild(e);
+</script>
