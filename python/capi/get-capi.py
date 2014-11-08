30042d97469022a8f1e88f9e1d544e2153c52e90
Robert <lum.robert@gmail.com>
2014-11-08 16:52:28 -0500

    writing a file called response.json with feed from http request

--- python/capi/get-capi.py
+++ python/capi/get-capi.py
@@ -1,4 +1,10 @@
 from urllib2 import Request, urlopen
+#import json
+
 request = Request("https://api.offerpop.com/v1/ugc/collections/6900/?access_token=1dDCBwoeNf1QgZkYaZSwSvjik7lDKV&page=1&page_size=100&social_platform=twitter%2Cinstagram%2Cvinetweet&claimed=yes&order=random_sort&seed=7&approval_status=app&media_type=video%2Cimage")
 response_body = urlopen(request).read()
 print response_body
+
+
+with open('capi/response.json', 'w') as outfile:
+  outfile.write(response_body)
