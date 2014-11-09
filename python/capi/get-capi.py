0bef01191a4cb5ae612c8f9562ba24b36588cfcc
Robert <lum.robert@gmail.com>
2014-11-08 22:01:59 -0500

    script is running through pages; validating if next href is not None. need to get each UGC with geo enabled to write to external json file

--- python/capi/get-capi.py
+++ python/capi/get-capi.py
@@ -7,9 +7,10 @@ def pullFeed(page):
     baseURL         = 'https://api.offerpop.com/v1/ugc/collections/'
     access_token    = '1dDCBwoeNf1QgZkYaZSwSvjik7lDKV'
     campaignId      = 6900
+    page_size       = 10
     next            = False
 
-    compiledRequest = baseURL + str(campaignId) + '/?access_token=' + access_token + '&page=' + str(page) + '&page_size=100&social_platform=twitter%2Cinstagram%2Cvinetweet&order=random_sort&seed=7&approval_status=app&media_type=video%2Cimage'
+    compiledRequest = baseURL + str(campaignId) + '/?access_token=' + access_token + '&page=' + str(page) + '&page_size=' + str(page_size) + '&social_platform=twitter%2Cinstagram%2Cvinetweet&order=random_sort&seed=7&approval_status=app&media_type=video%2Cimage'
 
     req             = Request(compiledRequest)
     res_body        = json.loads(urlopen(req).read().decode("utf-8"))
@@ -19,7 +20,7 @@ def pullFeed(page):
     storeUGC(filtered)
     # writeFile(res_body)
 
-    if '_links' in res_body:
+    if res_body['_links']['next']['href'] is not None:
         page += 1
         print 'there is another page' + str(page)
         pullFeed(page)
@@ -31,96 +32,9 @@ def storeUGC(data):
 
         if platform_data and content['media'] and platform_data['geo_data'] and platform_data['geo_data']['coordinates']:
             print 'hello'
-              #   geo_data  = platform_data.geo_data
-              #   images   = content.media.media_urls
-              #   network  = content.social_platform;
-              
-              #   newUGC = {
-              #       author          : content.author.username
-              #       ,avatar         : content.author.profile.avatar
-              #       ,caption        : content.text
-              #       ,latitude       : geo_data.coordinates.latitude
-              #       ,longitude      : geo_data.coordinates.longitude
-              #       ,large_image    : images.large_image
-              #       ,network        : network
-              #       ,timestamp      : new Date(content.created_on).toDateString()
-              #       ,platform_link  : platform_data.social_platform_original_url
-
-              # if (network === 'twitter') newUGC.platform_ref = content.platform_ref;
-
-              # ugcStorage['item' + ugcCounter] = newUGC;
 
 def writeFile(res):
     with open('capi/response.json', 'w') as outfile:
         outfile.write(res)
 
 pullFeed(page)
-
-
-    # opopMaps.mapManager.storeUGC(data['_embedded']['ugc:item']);
-
-    #             if (data['_links'].next.href) {
-    #                 var nextUrl = data['_links'].next.href
-    #                     ,pageIndex = nextUrl.indexOf('&page=')
-    #                     ,nextPage = nextUrl.slice(pageIndex + 6, nextUrl.length);
-
-    #                     console.log(nextPage);
-    #                     opopMaps.mapManager.pullFeed(nextPage);
-
-    #             }
-
-    #             if (ugcStorage['item1']){
-    #                 var first = ugcStorage['item1']
-    #                     ,coord = new google.maps.LatLng(first.latitude, first.longitude);
-
-    #                     googleMap.panTo(coord);
-    #             }
-
-    #         });
-
-    #     },
-    #     storeUGC : function(data){
-    #         console.log(data);
-
-    #           for (var i in data){
-    #             console.log(data[i])
-    #             var content         = data[i].content
-    #                 ,platform_data  = content.platform_data;
-
-    #               if (platform_data &&
-    #                   content.media &&
-    #                   platform_data.geo_data &&
-    #                   platform_data.geo_data.coordinates){
-
-    #                       var geo_data  = platform_data.geo_data
-    #                           ,images   = content.media.media_urls
-    #                           ,network  = content.social_platform;
-
-    #                           console.log(geo_data);
-
-    #                       // Store necessary UGC data into geoStore object
-    #                       var newUGC = {
-    #                                   author          : content.author.username
-    #                                   ,avatar         : content.author.profile.avatar
-    #                                   ,caption        : content.text
-    #                                   ,latitude       : geo_data.coordinates.latitude
-    #                                   ,longitude      : geo_data.coordinates.longitude
-    #                                   ,large_image    : images.large_image
-    #                                   ,network        : network
-    #                                   ,timestamp      : new Date(content.created_on).toDateString()
-    #                                   ,platform_link  : platform_data.social_platform_original_url
-    #                           // ,small_image : images.url
-    #                       }
-
-    #                       if (network === 'twitter') newUGC.platform_ref = content.platform_ref;
-
-    #                       // Store geo data in global array
-    #                       ugcStorage['item' + ugcCounter] = newUGC; // Store obj in global repo
-    #                       // Store geo data in heatMap array
-    #                       heatData.push(new google.maps.LatLng(geo_data.coordinates.latitude, geo_data.coordinates.longitude));
-
-    #                       // Create marker with data
-    #                       opopMaps.mapManager.createMarker(newUGC);
-    #                       ugcCounter++;
-    #           }
-    #       }   
