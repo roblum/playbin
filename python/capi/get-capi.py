be6a0e426af1873e0d83d2ec1e52b84bf0946d74
Robert <lum.robert@gmail.com>
2014-11-08 21:03:35 -0500

    sorted request in variables; write json file in its own function; will work out logic to paginate through content before writing to json

--- python/capi/get-capi.py
+++ python/capi/get-capi.py
@@ -1,10 +1,21 @@
 from urllib2 import Request, urlopen
-#import json
 
-request = Request("https://api.offerpop.com/v1/ugc/collections/6900/?access_token=1dDCBwoeNf1QgZkYaZSwSvjik7lDKV&page=1&page_size=100&social_platform=twitter%2Cinstagram%2Cvinetweet&claimed=yes&order=random_sort&seed=7&approval_status=app&media_type=video%2Cimage")
-response_body = urlopen(request).read()
-print response_body
+def pullFeed(page):
+    baseURL = 'https://api.offerpop.com/v1/ugc/collections/'
+    campaignId = 6900
+    access_token = '1dDCBwoeNf1QgZkYaZSwSvjik7lDKV'
+    next = False
 
+    compiledRequest = baseURL + str(campaignId) + '/?access_token=' + access_token + '&page=' + str(page) + '&page_size=100&social_platform=twitter%2Cinstagram%2Cvinetweet&order=random_sort&seed=7&approval_status=app&media_type=video%2Cimage'
 
-with open('capi/response.json', 'w') as outfile:
-  outfile.write(response_body)
+    req = Request(compiledRequest)
+    res_body = urlopen(req).read()
+    
+    writeFile(res_body)
+
+def writeFile(res):
+    with open('capi/response.json', 'w') as outfile:
+        outfile.write(res)
+
+pullFeed(1)
+
