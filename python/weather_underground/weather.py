4c0d37e4fea3841badc1f5c54411dc3ca485ac0f
Robert <lum.robert@gmail.com>
2014-11-11 20:49:15 -0500

    formatted request

--- python/weather_underground/weather.py
+++ python/weather_underground/weather.py
@@ -2,18 +2,25 @@ from urllib2 import Request, urlopen
 import json
 
 def pullFeed():	
-    requestString 	= 'http://api.wunderground.com/api/ed81498dd08a91f6/conditions/q/NY/New_York_City.json'
+	baseURL 		= 'http://api.wunderground.com/api'
+	apiKey 			= 'ed81498dd08a91f6'
+	condition 		= 'hourly'
+	state 			= 'NY'
+	city 			= 'New_York_City'
+	
+	requestString 	= "%(baseURL)s/%(apiKey)s/%(condition)s/q/%(state)s/%(city)s.json" % locals()
+	
+	req             = Request(requestString)
+	res_body        = json.loads(urlopen(req).read().decode("utf-8"))
 
-    req             = Request(requestString)
-    res_body        = json.loads(urlopen(req).read().decode("utf-8"))
-    
-    print req
-    print res_body
-    writeFile(res_body);
-    
+	print req
+	print res_body
+
+	writeFile(res_body)
+
 def writeFile(res_body):
-    cleaned = json.dumps(res_body, ensure_ascii=False)
-    with open('response.json', 'w') as outfile:
-        outfile.write(cleaned)
+	cleaned = json.dumps(res_body, ensure_ascii=False)
+	with open('response.json', 'w') as outfile:
+		outfile.write(cleaned)
 
 pullFeed();
