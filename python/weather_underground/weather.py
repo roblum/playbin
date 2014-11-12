b48501e8c03014ce3ca2e052acde5ad7672f76c3
Rob Lum <lum.robert@gmail.com>
2014-11-12 10:46:59 -0500

    fixed indenetations

--- python/weather_underground/weather.py
+++ python/weather_underground/weather.py
@@ -1,41 +1,41 @@
 from urllib2 import Request, urlopen
-from icons import *
+# from icons import *
 import json
 
 weather = [];
 
-def pullFeed():	
-	baseURL 		= 'http://api.wunderground.com/api'
+def pullFeed():
+	baseURL 			= 'http://api.wunderground.com/api'
 	apiKey 			= 'ed81498dd08a91f6'
 	condition 		= 'hourly'
 	state 			= 'NY'
 	city 			= 'New_York_City'
-	
-	requestString 	= "%(baseURL)s/%(apiKey)s/%(condition)s/q/%(state)s/%(city)s.json" % locals()
-	req             = Request(requestString)
-	
-	res_body        = json.loads(urlopen(req).read().decode("utf-8"))
+
+	requestString 		= "%(baseURL)s/%(apiKey)s/%(condition)s/q/%(state)s/%(city)s.json" % locals()
+	req             	= Request(requestString)
+
+	res_body        	= json.loads(urlopen(req).read().decode("utf-8"))
 	hourly 			= res_body['hourly_forecast']
 
 	inc = 1
-	
+
 	while inc <= 8:
 		weather.append({
 				'day'			: hourly[inc]['FCTTIME']['weekday_name_night']
 				,'time' 			: hourly[inc]['FCTTIME']['civil']
-				,'temp' 		: hourly[inc]['temp']['english']
-				,'condition' 	: hourly[inc]['wx']
-				,'icon' 		: hourly[inc]['icon_url']
+				,'temp' 			: hourly[inc]['temp']['english']
+				,'condition' 		: hourly[inc]['wx']
+				,'icon' 			: hourly[inc]['icon_url']
 			})
 		inc += 1
-		
+
 
 	writeFile(weather, hourly)
 
 def writeFile(res_body, hourly):
 	data = json.dumps(res_body, ensure_ascii=False)
 	partial = json.dumps(hourly, ensure_ascii=False)
-	
+
 	with open('response.json', 'w') as outfile:
 		outfile.write(data)
 	with open('hourly.json', 'w') as outfile:
