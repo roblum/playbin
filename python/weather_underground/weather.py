edc92f2999444baac396018f426e9cb3df650a40
Robert <lum.robert@gmail.com>
2014-11-11 21:58:19 -0500

    storing some info about the next 8 hours

--- python/weather_underground/weather.py
+++ python/weather_underground/weather.py
@@ -1,5 +1,8 @@
 from urllib2 import Request, urlopen
+from icons import *
 import json
+
+weather = [];
 
 def pullFeed():	
 	baseURL 		= 'http://api.wunderground.com/api'
@@ -9,18 +12,31 @@ def pullFeed():
 	city 			= 'New_York_City'
 	
 	requestString 	= "%(baseURL)s/%(apiKey)s/%(condition)s/q/%(state)s/%(city)s.json" % locals()
-	
 	req             = Request(requestString)
+	
 	res_body        = json.loads(urlopen(req).read().decode("utf-8"))
+	hourly 			= res_body['hourly_forecast']
 
-	print req
-	print res_body
+	inc = 1
+	
+	while inc <= 8:
+		weather.append({
+				'time' 			: hourly[inc]['FCTTIME']['civil']
+				,'temp' 		: hourly[inc]['temp']['english']
+				,'condition' 	: hourly[inc]['wx']
+			})
+		inc += 1
+		
 
-	writeFile(res_body)
+	writeFile(weather, hourly)
 
-def writeFile(res_body):
-	cleaned = json.dumps(res_body, ensure_ascii=False)
+def writeFile(res_body, hourly):
+	data = json.dumps(res_body, ensure_ascii=False)
+	partial = json.dumps(hourly, ensure_ascii=False)
+	
 	with open('response.json', 'w') as outfile:
-		outfile.write(cleaned)
+		outfile.write(data)
+	with open('hourly.json', 'w') as outfile:
+		outfile.write(partial)
 
 pullFeed();
